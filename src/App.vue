44af8e86b3fc69a139de24e2c2f2b1404929c9c9
Ruslan Babkin <ruslan.babkin01@gmail.com>
2023-05-21 11:13:00 +0300

    add select

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div :id="$style.app">
     <h2>{{ text }}</h2>
+    <CustomSelect :items="['name', 'label', 'salary']" />
     <CustomInput v-model="text" />
     <ApartmentsList :items="apartments">
       <template v-slot:apartment="{ apartment }">
@@ -22,12 +23,14 @@ import ApartmentsList from './components/apartment/ApartmentsList.vue'
 import ApartmentsItem from './components/apartment/ApartmentsItem.vue'
 import apartments from './components/apartment/apartments'
 import CustomInput from './components/shared/CustomInput.vue'
+import CustomSelect from './components/shared/CustomSelect.vue'
 export default {
   name: 'App',
   components: {
     ApartmentsList,
     ApartmentsItem,
     CustomInput,
+    CustomSelect,
   },
   data() {
     return {
