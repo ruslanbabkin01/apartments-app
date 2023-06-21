e42af41a9b4df6e7c8094984529ccbe8d575349a
Ruslan Babkin <ruslan.babkin01@gmail.com>
2023-06-21 10:27:14 +0300

    add protected route

--- src/router.js
+++ src/router.js
@@ -5,6 +5,7 @@ import ErrorPage from './pages/ErrorPage'
 import LoginPage from './pages/LoginPage'
 import RegisterPage from './pages/RegisterPage'
 import OrdersPage from './pages/OrdersPage'
+import store from './store'
 
 const routes = [
   {
@@ -16,21 +17,33 @@ const routes = [
     path: '/apartments/:id',
     component: ApartmentPage,
     name: 'apartment',
+    meta: {
+      requiresAuth: true,
+    },
   },
   {
     path: '/login',
     component: LoginPage,
     name: 'login',
+    meta: {
+      hideForAuth: true,
+    },
   },
   {
     path: '/register',
     component: RegisterPage,
     name: 'register',
+    meta: {
+      hideForAuth: true,
+    },
   },
   {
     path: '/orders',
     component: OrdersPage,
     name: 'orders',
+    meta: {
+      requiresAuth: true,
+    },
   },
   {
     path: '*',
@@ -39,6 +52,27 @@ const routes = [
   },
 ]
 
-const router = new VueRouter({ routes, mode: 'history' })
+const router = new VueRouter({
+  routes,
+  mode: 'history',
+})
+
+router.beforeEach((to, from, next) => {
+  const isLoggedIn = store.getters['auth/isLoggedIn']
+
+  if (to.matched.some(record => record.meta.requiresAuth)) {
+    if (!isLoggedIn) {
+      next({ name: 'login' })
+    }
+  }
+
+  if (to.matched.some(record => record.meta.hideForAuth)) {
+    if (isLoggedIn) {
+      next({ name: 'home' })
+    }
+  }
+
+  next()
+})
 
 export default router
